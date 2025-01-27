4a55a1bc74915aab0f608675ec233da5e8c71736
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2025-01-27 13:38:39 +0100

    create database and add customer table.

---
+++ EcoCarpet/EcoCarpet.Server/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcoCarpet.Server.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string PasswordHash { get; set; }
+    }
+}
--- EcoCarpet/EcoCarpet.Server/Program.cs
+++ EcoCarpet/EcoCarpet.Server/Program.cs
@@ -1,11 +1,20 @@
+using EcoCarpet.Server.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register the DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
