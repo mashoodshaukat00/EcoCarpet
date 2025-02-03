ba7099605b369b994221e5c68210f0ad6c899ef8
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2025-02-03 12:09:15 +0100

    integrate tailwindcss

--- EcoCarpet/ecocarpet.client/src/App.jsx
+++ EcoCarpet/ecocarpet.client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     
     return (
         <div>
-            <h1 id="tableLabel">Registration</h1>
+            <h1 className='text-3xl font-bold underline text-green-400'>Registration</h1>
             <p>This component demonstrates fetching data from the server.</p>
            
             <RegisterForm />
