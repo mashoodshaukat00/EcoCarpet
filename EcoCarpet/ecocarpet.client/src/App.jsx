ebb93660536d22b3aa94d8ac9775f673088df64d
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2025-02-04 10:40:24 +0100

    add routing, create new components.

--- EcoCarpet/ecocarpet.client/src/App.jsx
+++ EcoCarpet/ecocarpet.client/src/App.jsx
@@ -5,22 +5,33 @@ import ProductList from './components/ProductList/ProductList';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 import RegistrationForm from './components/Registratioin/RegistrationForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home/Home';
+import NoPage from './components/NoPage/NoPage';
 
 function App() {
     
-    return (        
+    return (    
+        <BrowserRouter>
             <div className="flex flex-col min-h-screen">
-                <header className="bg-gray-800 text-white p-4">
+               
                     <Nav/>
-                </header>
-                    <main className="flex-grow bg-gray-100 p-4">
-                {/*<RegisterForm />*/}
-                <RegistrationForm/>
-                        {/*<LoginForm />*/}
-                        {/*<ProductList />*/}
-                </main>
-                        <Footer/>
+            <main className="flex-grow bg-gray-100 p-4">
+                
+                    <Routes>
+                       
+                            <Route path= "/"element={<Home/> } />
+                            <Route path="/login" element={<LoginForm />} />
+                            <Route path="/signup" element={<RegistrationForm />} />
+                            <Route path="/products" element={<ProductList />} />
+                            <Route path="*" element={<NoPage />} />
+                        
+                    </Routes>
+                
+            </main>
+                <Footer/>
             </div>
+        </BrowserRouter>
     );   
 }
 
--- EcoCarpet/ecocarpet.client/src/components/Nav/Nav.jsx
+++ EcoCarpet/ecocarpet.client/src/components/Nav/Nav.jsx
@@ -1,19 +1,22 @@
-
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className="max-w-7xl mx-auto flex justify-between items-center">
-            <div className="flex items-center">
-                <img src="/src/assets/logo.png" alt="Logo" className="h-8 w-8 sm:h-10 sm:w-10 lg:h-12 lg:w-12 mr-2" />
-                <div className="text-xl font-bold">EcoCarpet</div>
-            </div>
-            <ul className="flex space-x-4">
-                <li><a href="./Login" className="hover:underline">Login</a></li>
-                <li><a href="./RegistrationForm" className="hover:underline">Signup</a></li>
-                <li><a href="#" className="hover:underline">About</a></li>
-                <li><a href="#" className="hover:underline">Contact</a></li>
-            </ul>
-        </nav>
+        <header className="bg-gray-800 text-white p-4">
+            <nav className="max-w-7xl mx-auto flex justify-between items-center">
+                <div className="flex items-center">
+                    <img src="/src/assets/logo.png" alt="Logo" className="h-8 w-8 sm:h-10 sm:w-10 lg:h-12 lg:w-12 mr-2" />
+                    <div className="text-xl font-bold"><Link to="/" className="hover:underline">EcoCarpet</Link></div>
+                </div>
+                <ul className="flex space-x-4">
+                    <li><Link to="/login" className="hover:underline">Login</Link></li>
+                    <li><Link to="/signup" className="hover:underline">Signup</Link></li>
+                    <li><Link to="/products" className="hover:underline">Products</Link></li>
+                    <li><Link to="*" className="hover:underline">About us</Link></li>                    
+                </ul>
+            </nav>                   
+        </header>
+       
     )
 }
 
