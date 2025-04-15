34100e8ee224b3b60e796102deb46d1fe9e9b748
rashmisinha07 <rashmisinha.ranchi@gmail.com>
2025-04-15 10:40:32 +0200

    carousel completed

--- EcoCarpet/ecocarpet.client/src/components/Carousel/Carousel.jsx
+++ EcoCarpet/ecocarpet.client/src/components/Carousel/Carousel.jsx
@@ -2,8 +2,32 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../Carousel/Style.css';
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 function Carousel() {
+    const [carpets, setCarpets] = useState([]);
+    const navigate = useNavigate();
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    useEffect(() => {
+        const fetchCarpets = async () => {
+           
+            try {
+                const response = await fetch(`${apiUrl}/Carpet`);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                const data = await response.json();
+                setCarpets(data);
+            } catch (err) {               
+                console.error('Error fetching carpets:', err);
+            } 
+        };
+
+        fetchCarpets();
+    }, [apiUrl]);
+
     const settings = {
         dots: true,
         infinite: true,
@@ -11,9 +35,9 @@ function Carousel() {
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000,
+        autoplaySpeed: 2000,
         className: 'Style',
-        centerMode: true,
+        centerMode: false,
         centerPadding: "60px",
         responsive: [
             {
@@ -26,7 +50,7 @@ function Carousel() {
                 }
             },
             {
-                breakpoint: 600,
+                breakpoint: 780,
                 settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
@@ -37,7 +61,8 @@ function Carousel() {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
+                    dots:false
                 }
             }
         ]
@@ -47,16 +72,16 @@ function Carousel() {
         <div className='w-3/4 m-auto'>
             <div className="mt-20">
                 <Slider {...settings}>
-                    {data.map((d) => (
-                        <div key={d.name} className="bg-white h-[450px] text-black flex flex-col">
+                    {carpets.map((carpet) => (
+                        <div key={carpet.carpetID} className="bg-white h-[450px] text-black flex flex-col">
                             <div className='h-56 bg-indigo-500 flex justify-center items-center'>
-                                <img src={d.img} alt="" className='h-full w-full object-cover'/>
+                                <img src={`/images/${carpet.imgName}.jpg`} alt={carpet.name} className='h-full w-full object-cover'/>
                             </div>
-
-                            <div className="flex flex-col items-center justify-center gap-4 p-4">
-                                <p className="text-xl font-semibold">{d.name}</p>
-                                <p className="text-center">{d.descriptions}</p>
-                                <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
+                            <div className="description-container flex flex-col items-center gap-4 h-full">
+                              <p className="text-xl font-semibold">{carpet.name}</p>
+                              <p className="description flex-grow overflow-y-auto text-center max-h-[100px]">{carpet.descriptions}</p>
+                                <button onClick={() => navigate(`/products/${carpet.carpetID}`)}
+                                    className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl mt-2'>Read More</button>
                             </div>
                         </div>
                     ))}
@@ -66,34 +91,5 @@ function Carousel() {
         </div>
     );
 }
-
-const data = [
-    {
-        name: `John Morgan`,
-        img: `../../../public/images/faux_fur_rug.jpg`,
-        descriptions:'Soft, luxurious feel, plush pile,warmth.',
-    },
-    {
-        name: `Bohemian Chic Rug`,
-        img: `../../../public/images/bohemian_chic_rug.jpg`,
-        descriptions: `Eclectic design, vibrant colors.`
-    },
-    {
-        name: `Sheepskin Rug`,
-        img: `../../../public/images/sheepskin_rug.jpg`,
-        descriptions: `Natural, cozy texture, soft and warm.`
-    },
-    {
-        name: `Moroccan Trellis`,
-        img: `../../../public/images/moroccan_trellis.jpg`,
-        descriptions: `Traditional pattern, vibrant colors.`
-    },
-    {
-        name: `Abstract Watercolor Rug`,
-        img: `../../../public/images/abstract_watercolor_rug.jpg`,
-        descriptions: `Modern, artistic design, vibrant colors,low pile.`
-    },
-
-];
 
 export default Carousel;
